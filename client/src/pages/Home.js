b144b841d0ee6e1a66b562b92b7dc9b5175b7bd0
Tsaikkari <kirsi.trospe@gmail.com>
2021-12-17 00:33:05 +0100

    prepare for deployment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import sun from '../images/sun.png'
 
 export default function Home() {
   return (
-    <div className='home-container'>
+    <div
+      className='home-container'
+      style={{
+        backgroundImage: `url(${sun})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <h1 className='homepage-header'>DAILY DOSE OF DOPAMINE</h1>
     </div>
   )
