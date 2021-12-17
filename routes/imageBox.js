0e1b4a51adfe87617295b33bdfbbce8c942355a1
Tsaikkari <kirsi.trospe@gmail.com>
2021-12-17 08:47:51 +0100

    image upload

--- routes/imageBox.js
+++ routes/imageBox.js
@@ -17,7 +17,7 @@ router.get('/', isAuthenticated, async (req, res, next) => {
 })
 
 // upload image to Cloudinary
-router.post('/', uploader.single('image'), (req, res, next) => {
+router.post('/upload', uploader.single('image'), (req, res, next) => {
   try {
     res.json({ secure_url: req.file.path })
   } catch (err) {
