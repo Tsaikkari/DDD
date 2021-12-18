3c153448d449b300b374d9b4bb0a34835dda1f90
Tsaikkari <kirsi.trospe@gmail.com>
2021-12-19 00:32:43 +0100

    Update AddVideo.js

--- client/src/components/AddVideo.js
+++ client/src/components/AddVideo.js
@@ -46,14 +46,14 @@ export default function AddVideo(props) {
             onChange={(e) => setTitle(e.target.value)}
           />
         </Form.Group>
-        {/* <Form.Group controlId='url' className='mb-3 url-input'>
+        <Form.Group controlId='url' className='mb-3 url-input'>
           <Form.Control
             type='text'
             placeholder='Enter YouTube Video URL'
             value={url}
             onChange={handleUrl}
           ></Form.Control>
-        </Form.Group> */}
+        </Form.Group>
         <Form.Group>
           <Button type='submit' className='save-btn add-video-btn'>
             Save
